ecc3ec147bbaad79334a511e291c16cf06e238f0
fdel15 <frankdelpidio@gmail.com>
2021-01-29 05:52:09 -0500

    finish testing chapter

---
+++ travel-packing/stories/Category.stories.js
@@ -0,0 +1,35 @@
+import { action } from '@storybook/addon-actions';
+import Category from '../src/Category.svelte';
+import '../public/global.css';
+
+export default { title: 'Category' }
+
+function getOptions(items) {
+    const category = { id: 1, name: 'Clothes', items }
+    return {
+        Component: Category,
+        props: {
+            category,
+            categories: {
+                [category.id]: category
+            },
+            dragAndDrop: {},
+            show: 'all'
+        },
+        on: { delete: action('category delete dispatched') }
+    }
+}
+
+export const empty = () => getOptions({});
+export const nonEmpty = () => getOptions({
+    1: { id: 1, name: 'socks', packed: true },
+    2: { id: 2, name: 'shoes', packed: false }
+})
+
+
+// export const empty = () => getOptions({});
+// export const nonEmpty = () =>
+// getOptions({
+// 1: {id: 1, name: 'socks', packed: true},
+// 2: {id: 2, name: 'shoes', packed: false}
+// });
